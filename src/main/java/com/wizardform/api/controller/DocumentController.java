17c54d65243abfda47c6f1cb4aa1427b24618213
ashutosh2706 <imashutosh2706@gmail.com>
2024-11-23 21:08:28 +0530

    add produces type for GetMapping('/download')

--- src/main/java/com/wizardform/api/controller/DocumentController.java
+++ src/main/java/com/wizardform/api/controller/DocumentController.java
@@ -9,6 +9,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,7 +32,7 @@ public class DocumentController {
         this.resourceLoader = resourceLoader;
     }
 
-    @GetMapping("download/{documentId}")
+    @GetMapping(value = "download/{documentId}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> downloadDocument(@PathVariable long documentId) throws FileDetailsNotFoundException {
 
